0ed7dfd87aa44544094834820417e0bd4c2907d0
Bekof257 <behzodbekpolatov5@gmail.com>
2023-09-26 22:45:50 +0500

    Upd 1

---
+++ src/services/article.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'b89d170c4bmsh19d6fdb30f32c9dp1ea851jsn9dd15567b775');
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
